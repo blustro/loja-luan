ad1f45619521ebd8df1c49c774fe8f0e18d4db2c
Bruno Lustro <brunolustro@gmail.com>
2026-07-18 14:07:01 +0100

    atualizara var da secret key do stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,12 +1,12 @@
 import 'server-only';
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY não foi definida no .env.local');
+if (!process.env.NEXT_STRIPE_SECRET_KEY) {
+  throw new Error('NEXT_STRIPE_SECRET_KEY não foi definida no .env.local');
 }
 
 // Criamos uma instância única para toda a aplicação
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY, {
   apiVersion: '2026-06-24.dahlia', // Pode manter a versão padrão ou atualizar se necessário
   typescript: true,
 });
